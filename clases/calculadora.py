71824895672697001dc491a194b381c2e76f50dd
untalemi <emirmz1352@gmail.com>
2022-11-03 17:34:09 -0600

    BUG

--- clases/calculadora.py
+++ clases/calculadora.py
@@ -2,6 +2,3 @@ class Calculadora:
     def __init__(self, num1, num2) -> None:
         self._num1 = num1;
         self._num2 = num2;
-
-    def __str__(self):
-        return str(self._num1 + self._num2)
